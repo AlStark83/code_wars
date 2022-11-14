f95b4f35b655428cc94bfb3d9fb7908e73139e90
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2022-11-13 22:22:10 -0600

    Create 5_Whitout_Numbers.js
    File 5_Whitout_Numbers.js

---
+++ Resueltos/5_Without_Numbers.js
@@ -0,0 +1,23 @@
+//DESCRIPTION:
+//Write a function that always returns 5
+//Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+//Good luck :)
+
+function unusualFive() {
+    let b;
+    let c;
+    let d;
+    let e;
+    let f;
+    const a = [b,c,d,e,f];
+    return a.length
+}
+// // 2da version
+// let abcde = "abcde"
+
+// let nuevoNumero5 = () => abcde.length
+
+//https://www.codewars.com/kata/59441520102eaa25260000bf/solutions/javascript?filter=all&sort=best_practice&invalids=false
+
+unusualFive=_=>'     '.length
+
