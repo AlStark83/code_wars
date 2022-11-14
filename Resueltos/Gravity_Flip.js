6182ba4ebc650fc8b535da4850e130afc479590e
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2022-11-13 19:48:03 -0600

    Create Gravity_Flip
    New File Gravity_Flip.js

---
+++ Resueltos/Gravity_Flip.js
@@ -0,0 +1,18 @@
+//DESCRIPTION:
+//If you've completed this kata already and want a bigger challenge, here's the 3D version
+// Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+// There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+
+const flip=(d, a)=>{
+  //TODO
+  if (d === "R"){
+      return a.sort(function(a, b) {return a - b})
+  } else if (d === "L"){
+      return a.sort(function(a, b){return b - a})
+  }
+  return 
+}
+
+//https://www.codewars.com/kata/5f70c883e10f9e0001c89673/solutions/javascript?filter=me&sort=best_practice&invalids=false
+
+// const flip = (d, a) => a.sort((x, y) => d === 'R' ? x - y : y - x);
